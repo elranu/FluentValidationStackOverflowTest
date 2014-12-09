d86f2e14c054637a17756e2e2ea669f16995e3c1
Mariano Julio Vicario <elranu@gmail.com>
2014-12-09 17:45:14 -0300

    Stackoverflow test

---
+++ FluentValidationStackOverflowTest/Tests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+using NUnit.Framework;
+
+namespace FluentValidationStackOverflowTest
+{
+    public class GridLayoutModuleDataModel
+    {
+        public List<GridLayoutRowModel> Rows { get; set; }
+    }
+
+    public class GridLayoutModel
+    {
+        public List<GridLayoutRowModel> Rows { get; set; }
+    }
+
+    public class GridLayoutRowModel
+    {
+        public List<GridLayoutModuleDataModel> Modules { get; set; }
+    }
+
+    public class GridLayoutModelValidator : AbstractValidator<GridLayoutModel>
+    {
+        public GridLayoutModelValidator()
+        {
+            RuleFor(layout => layout.Rows).NotEmpty().SetCollectionValidator(new GridLayoutRowModelValidator());
+        }
+    }
+
+    public class GridLayoutRowModelValidator : AbstractValidator<GridLayoutRowModel>
+    {
+        public GridLayoutRowModelValidator()
+        {
+            RuleFor(row => row.Modules).Cascade(CascadeMode.Continue).NotEmpty()
+                .SetCollectionValidator(new GridLayoutModuleDataModelValidator());
+        }
+    }
+
+    public class GridLayoutModuleDataModelValidator : AbstractValidator<GridLayoutModuleDataModel>
+    {
+        public GridLayoutModuleDataModelValidator()
+        {
+            RuleFor(module => module.Rows)
+                .Cascade(CascadeMode.StopOnFirstFailure)
+                .SetCollectionValidator(new GridLayoutRowModelValidator())
+                .When(x => x.Rows.Count > 0);
+        }
+    }
+
+    [TestFixture]
+    public class ValidatorTests
+    {
+        [Test]
+        public void BasicRecursiveTest()
+        {
+            var model = new GridLayoutModel();
+            var validator = new GridLayoutModelValidator();
+            validator.ValidateAndThrow(model); //this throws a stackoverflow exception
+        }
+    }
+}
